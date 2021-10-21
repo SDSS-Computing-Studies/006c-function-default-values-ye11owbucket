7a75a8eb4c3e17981f78a9d844bdfd082aaa525b
#1 wabungus fan <90716044+ye11owbucket@users.noreply.github.com>
2021-10-21 14:47:21 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -22,9 +22,8 @@ assert sentence("Hiya","Casey","Have you enjoyed your meal") == "Hiya Casey. Hav
 (2 points) 
 """
 
-def sentence():
-  return ""
-
+def sentence(a = "Hello", b = "Benjamin.", c = "How are you"):
+  return f"{a} {b} {c}"
 
 if __name__ == "__main__":
   assert sentence("Hello") == "Hello Benjamin. How are you"

6c4e95870be4586f48722fee417bb6f48555ab50
#1 wabungus fan <zacharym234@deltalearns.ca>
2022-01-25 14:19:16 -0800

    Update task1.py

--- task1.py
+++ task1.py
@@ -21,11 +21,9 @@ assert sentence("Hiya","Casey","Have you enjoyed your meal") == "Hiya Casey. Hav
 
 (2 points) 
 """
-def sentence():
-  return ""
 
 def sentence(a = "Hello", b = "Benjamin.", c = "How are you"):
   return f"{a} {b} {c}"
-
+print(sentence("Hiya","Casey","Have you enjoyed your meal"))
 if __name__ == "__main__":
   assert sentence("Hello") == "Hello Benjamin. How are you"
